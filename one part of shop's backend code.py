324cdf859d745c5d8e1de20b4fd5b8c75a838cff
John <karimboyev771@gmail.com>
2025-12-06 21:19:37 +0500

    Ko'pincha magazinlarda ishlatiladigan dasturning kichik qismi
    if else elif for in range qatnashgan

---
+++ one part of shop's backend code.py
@@ -0,0 +1,13 @@
+mahsulotlar=['sut','qatiq','anor','banan','olma']
+savat=[]
+print('3 ta mahsulot qo\'shing')
+for n in range(3):
+    savat.append(input(f'{n+1}-mahsulot: '))
+if savat:
+    for mahsulot in savat:
+        if mahsulot in mahsulotlar:
+            print(f'Do\'konimizda {mahsulot} bor!')
+        else:
+            print(f'Do\'konimizda {mahsulot} mavjud emas')
+else:
+    print('Savat bo\'sh')
