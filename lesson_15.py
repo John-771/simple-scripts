9b788107794d6441e940528d44261819bd42d07a
John <karimboyev771@gmail.com>
2025-12-10 22:12:50 +0500

    Fayl yangilandi!
    Dasturga bir nechta davlatlar qo'shildi

--- lesson_15.py
+++ lesson_15.py
@@ -10,10 +10,26 @@ f_country={'brazilia':'Charles Oliviera',
            'armaniston':'Arman Sarukiyan',
            'gruziya':'Merab Dvalashvili',
            'amerika':'Dastin Porier',
-           'irlandiya':'Konor Makregor'}
+           'irlandiya':'Konor Makregor',
+           'avstraliya':'Aleksandr Valkanovskiy',
+           'qozog\'iston':'Shavkat Rahmanov',
+           'qirg\'iziston':'Myktybek Oralbay',
+           'italiya':'Marvin Vettori',
+           'kanada':'Georg Sent-Pier (GSP)',
+           'xitoy':'Song Yadong',
+           'koreya':'Chan Sung Jung',
+           'yaponiya':'Kazushi Sakuraba',
+           'tailand':'Loma Lookboonmee',
+           'avg\'oniston':'Nasrat Haqparast',
+           'afrika':'Kamaru Usman',
+           'ozarbayjon':'Rafael Fiziev',
+           'Germaniya':'Dennis Siver',
+           'polsha':'Jan Blachowicz',
+           'fransiya':'Kiryl Gane',
+           'turkiya':'Jengiz Nurmagamedov'}
 davlat=input('Qaysi davlatning UFC jangchisini ko\'rishni xoxlaysiz? ').lower()
 jangchi=f_country.get(davlat)
 if davlat in f_country.keys():
-    print(f'{davlat.title()}lik jangchilardan biri bu {jangchi.title()}')
+    print(f'{davlat.title()}lik mashxur UFC jangchilaridan biri bu {jangchi.title()}')
 else:
     print('Bu davlat sporti haqida bizda ma\'lumot mavjud emas!')
